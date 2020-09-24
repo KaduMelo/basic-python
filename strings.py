70d3a210c34398bfab4548a28fac108007f2e58f
Carlos Eduardo <carlos.silva@gt4w.com.br>
2020-09-24 19:40:41 -0300

    feat: Impl with url slicing with printout of argument values

--- strings.py
+++ strings.py
@@ -4,3 +4,11 @@ print(argumento[index + 1:])
 
 list_argumento = argumento.split("=")
 print(list_argumento)
+
+url = 'www.bytebank.com.br/cambio?valor=1500&moedaOrigem=real&moedaDestino=dolar'
+pega_argumentos_url = url.find("?")
+list_argumentos = url[pega_argumentos_url + 1:].split("&")
+
+for argumento in list_argumentos:
+    index = argumento.find("=")
+    print(argumento[index + 1:])
